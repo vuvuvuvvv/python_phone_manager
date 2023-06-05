dd45acbbeada5e247e9575269b152ebad923dbd1
vuvuvuvvv <nguyenvu260502@gmail.com>
2023-06-06 04:30:29 +0700

    fix and update main code

--- main.py
+++ main.py
@@ -2,59 +2,108 @@ from modules import *
 from datetime import *
 import math
 
-DanhSachSanPham = []
+#
+# Mo hinh dictionary danh sach chua dien thoai
+#   DanhSachSanPham = {
+#       id1 = [
+#           dienthoai1,
+#           dienthoai2,
+#           ...
+#       ],
+#       id2 = [...],
+#   }
+#
+DanhSachSanPham = {}
 status = {
     0: "Khong hoat dong",
     1: 'Hoat dong'
 }
 
-# class Memory:
-#     def __init__(self,storage,ram):
-#         self.storage = storage
-#         self.ram = ram
-    
-#     def __str__(self):
-#         return f"Dung luong: {self.storage}GB\nRAM: {self.ram}GB\n"
-
-#     def __del__ (self):
-#         print("Đã xóa!")
-
 class DienThoai:
-    id = 0  # Auto increment
-    ten = None
-    hang = None
-    dungluong = None
-    soluong = None
-    gia = None
-    ram = None
-    status = 1 #Automatically activated after creation
-    nam_sxuat = None
+    count = 0  # Auto increment
 
     def __init__(self):
-        DienThoai.id += 1
+        DienThoai.count += 1
+        self.id = DienThoai.count
+        self.ten = None
+        self.hang = None
+        self.dungluong = None
+        self.ram = None
+        self.soluong = None
+        self.gia = None
+        self.nam_sxuat = None
+        self.status = 1 #Automatically activated after creation
+
+    def get_id(self):
+        return self.id
+    
+    def set_ten(self, ten):
+        self.ten = ten
+    
+    def get_ten(self):
+        return self.ten
+    
+    def set_hang(self, hang):
+        self.hang = hang
+    
+    def get_hang(self):
+        return self.hang
+    
+    def set_dungluong(self, dungluong):
+        self.dungluong = dungluong
+    
+    def get_dungluong(self):
+        return self.dungluong
+    
+    def set_soluong(self, soluong):
+        self.soluong = soluong
+    
+    def get_soluong(self):
+        return self.soluong
+    
+    def set_gia(self, gia):
+        self.gia = gia
+    
+    def get_gia(self):
+        return self.gia
+    
+    def set_ram(self, ram):
+        self.ram = ram
+    
+    def get_ram(self):
+        return self.ram
+    
+    def set_nam_sxuat(self, nam_sxuat):
+        self.nam_sxuat = nam_sxuat
+    
+    def get_nam_sxuat(self):
+        return self.nam_sxuat
 
     def __str__(self):
-        result = f"\nDien thoai: {self.ten}"\
-            f"\nHang: {self.hang}" if self.hang else "" \
-            f"\nSo luong: {self.soluong}" if self.soluong else "" \
-            f"\nGia: {self.gia}" if self.gia else "" \
-            f"\nDung luong: {self.dungluong}" if self.dungluong else "" \
-            f"\nRAM: {self.ram}" if self.ram else "" \
-            f"\nNam san xuat: {self.nam_sxuat}" if self.nam_sxuat else "" 
+        result = f"Dien thoai: {self.get_ten()}"
+        if self.hang is not None:
+            result += f"\nHang: {self.get_hang()}"
+        if self.dungluong is not None:
+            result += f"\nDung luong: {self.get_dungluong()} GB"
+        if self.ram is not None:
+            result += f"\nRAM: {self.get_ram()} GB"
+        if self.gia is not None:
+            result += f"\nGia: {format(self.get_gia(), ',d').replace(',', '.')}VND"
+        if self.soluong is not None:
+            result += f"\nSo luong: {self.get_soluong()}"
+        if self.nam_sxuat is not None:
+            result += f"\nNam san xuat: {self.get_nam_sxuat()}"
         return result
 
-    # def sua_dien_thoai(self, id, new_dien_thoai):
-    #     for i in range(len(self.dien_thoai_list)):
-    #         if self.dien_thoai_list[i].id == id:
-    #             self.dien_thoai_list[i] = new_dien_thoai
-    #             break
+    def get_list_thongtin_dienthoai(self):
+        return [self.id, self.ten, self.hang, self.dungluong, self.ram, self.soluong, self.gia, self.nam_sxuat, status[self.status]]
 
-    def __del__ (self, id):
+    def __del__ (self):
         # for dien_thoai in self.dien_thoai_list:
         #     if dien_thoai.id == id:
         #         self.dien_thoai_list.remove(dien_thoai)
         #         break
-        return
+        return 
 
 
 def nhapDienThoai():
@@ -67,138 +116,107 @@ def nhapDienThoai():
     print('----------------------------')
     print('Nhap thong tin cho dien thoai:')
     for i in range(0, sl_dienthoai):
-        tmp_product = DienThoai()
+        dienthoai = DienThoai()
+        #
+        #Khoi tao key va value luu du lieu dien thoai vao dictionary DanhSachSanPham
+        # Key: Id dien thoai
+        # Value: List du lieu dien thoai
+        #
+        DanhSachSanPham[dienthoai.get_id()] = []
         print(f'San pham thu {i + 1 if i > 0 else "nhat"}:')
-        tmp_product.id = DienThoai.id
-
-        tmp_product.ten = input("Nhap ten: ")
-        while len(tmp_product.ten) <= 0 or len(tmp_product.ten) > 100:
+        #Nhap ten
+        tmp_ten = input("Nhap ten: ")
+        while len(tmp_ten) <= 0 or len(tmp_ten) > 100:
             print("Ten khong duoc de trong va khong qua 100 ky tu!")
-            tmp_product.ten = input("Nhap ten san pham: ")
+            tmp_ten = input("Nhap ten san pham: ")
+        dienthoai.set_ten(tmp_ten)
 
-        tmp_product.hang = input("Nhap ten hang san xuat: ")
-        while len(tmp_product.ten) <= 0 or len(tmp_product.ten) > 100:
+        #Nhap hang san xuat
+        tmp_hang = input("Nhap ten hang san xuat: ")
+        while len(tmp_hang) <= 0 or len(tmp_hang) > 100:
             print("Ten khong duoc de trong va khong qua 100 ky tu!")
-            tmp_product.ten = input("Nhap ten hang san xuat: ")
-            
+            tmp_hang = input("Nhap ten hang san xuat: ")
+        dienthoai.set_hang(tmp_hang)
+
+        #
+        # Khoi tao vong lap lap theo so loai dung luong, moi loai dung luong co 1 hoac nhieu loai ram khac nhau
+        # Moi 1 loai dien thoai co dung luong va RAM lai co 1 muc gia va so luong khac
+        #
+        #Nhap so loai dung luong
         print('----------------------------')
         print("Nhap so loai dung luong cho:")
-        print(str(tmp_product))
-        lable_input = "So luong: "
-        q_dungluong = input(lable_input)
-        q_dungluong = ValidateAmountInputForm(q_dungluong, lable_input)
-        list_dungluong = []
-        for j in range(0, q_dungluong):
+        print(str(dienthoai))
+        lable_input = "So luong loai dung luong: "
+        so_dungluong = input(lable_input)
+        so_dungluong = ValidateAmountInputForm(so_dungluong, lable_input)
+        list_dungluong = [] #Khoi tao mang dung luong cho du lieu dien thoai hien tai = []
+        for j in range(0, so_dungluong):
+            #Nhap dung luong
             print('----------------------------')
-            print('Nhap dung luong cho: ')
-            print(str(tmp_product))
+            print(f'Nhap loai dung luong thu {"nhat" if j == 0 else j+1}: ')
             lable_input = "Nhap dung luong (GB): "
-            tmp_product.dungluong = input(lable_input)
-            tmp_product.dungluong = ValidateAmountInputForm(tmp_product.dungluong, lable_input)
-            while tmp_product.dungluong in list_dungluong:
-                print(f"Dung luong {tmp_product.dungluong}GB da co san!")
-                tmp_product.dungluong = input(lable_input)
-                tmp_product.dungluong = ValidateAmountInputForm(tmp_product.dungluong, lable_input)
-            list_dungluong.append(tmp_product.dungluong)
-
+            tmp_dungluong = input(lable_input)
+            tmp_dungluong = ValidateAmountInputForm(tmp_dungluong, lable_input)
+            while True:
+                if tmp_dungluong in list_dungluong:
+                    print(f"Dung luong {tmp_dungluong}GB da co san!")
+                    tmp_dungluong = input(lable_input)
+                    tmp_dungluong = ValidateAmountInputForm(tmp_dungluong, lable_input)
+                else:
+                    dienthoai.set_dungluong(tmp_dungluong)
+                    list_dungluong.append(tmp_dungluong)
+                    break
+            #Nhap so loai RAM cho loai dien thoai co dung luong vua nhap
             print('----------------------------')
             print("Nhap so loai RAM cho:")
-            print(str(tmp_product))
-            lable_input = "So luong: "
-            q_ram = input(lable_input)
-            q_ram = ValidateAmountInputForm(q_ram, lable_input)
+            print(str(dienthoai))
+            lable_input = "So luong loai RAM: "
+            so_ram = input(lable_input)
+            so_ram = ValidateAmountInputForm(so_ram, lable_input)
             list_ram = []
-            for k in range(0, q_ram):
+            for k in range(0, so_ram):
+                #Nhap RAM
                 print('----------------------------')
-                print('Nhap RAM cho: ')
-                print(str(tmp_product))
+                print(f'Nhap loai RAM thu {"nhat" if k == 0 else k+1}: ')
                 lable_input = "Nhap RAM (GB): "
-                tmp_product.ram = input(lable_input)
-                tmp_product.ram = ValidateAmountInputForm(tmp_product.ram, lable_input)
-                while tmp_product.ram in list_ram:
-                    print(f"Ram {tmp_product.ram}GB da co san!")
-                    tmp_product.ram = input(lable_input)
-                    tmp_product.ram = ValidateAmountInputForm(tmp_product.ram, lable_input)
+                tmp_ram = input(lable_input)
+                tmp_ram = ValidateAmountInputForm(tmp_ram, lable_input)
+                while True:
+                    if tmp_ram in list_ram:
+                        print(f"RAM {tmp_ram}GB da co san!")
+                        tmp_ram = input(lable_input)
+                        tmp_ram = ValidateAmountInputForm(tmp_ram, lable_input)
+                    else:
+                        dienthoai.set_ram(tmp_ram)
+                        list_ram.append(tmp_ram)
+                        break
                 
+                #Hoan thien gia ban, so luong, nam san xuat cho dien thoai 
+                print('----------------------------')
+                print('Hoan thien thong tin cho: ')
+                print(str(dienthoai))
+                #Gia ban
                 lable_input = "Gia ban (VND): "
-                tmp_product.gia = input(lable_input)
-                tmp_product.gia = ValidateAmountInputForm(tmp_product.gia, lable_input,1000000)
-
+                tmp_gia = input(lable_input)
+                tmp_gia = ValidateAmountInputForm(tmp_gia, lable_input,1000000)
+                dienthoai.set_gia(tmp_gia)
+                #So luong
                 lable_input = "So luong: "
-                tmp_product.soluong = input(lable_input)
-                tmp_product.soluong = ValidateAmountInputForm(tmp_product.soluong, lable_input)
-
-
+                tmp_soluong = input(lable_input)
+                tmp_soluong = ValidateAmountInputForm(tmp_soluong, lable_input)
+                dienthoai.set_soluong(tmp_soluong)
+                #Nam san xuat
                 lable_input = "Nam san xuat: "
-                tmp_product.nam_sxuat = input(lable_input)
-                tmp_product.nam_sxuat = ValidateAmountInputForm(tmp_product.nam_sxuat, lable_input,1900, datetime.now().year)
-
-                DanhSachSanPham.append[tmp_product]
-
-# class User :
-#     count = 0
-
-#     def __init__(self, ten,ngaysinh,sdt,email,gioitinh,diachi):
-
-
-#         self.ten = ten
-#         self.ngaysinh = ngaysinh
-#         self.sdt =sdt
-#         self.email = email
-#         self.gioitinh = gioitinh
-#         self.diachi = diachi
-
-#         User.count += 1
-
-#     def set_id(self, ten):
-#         self.ten = ten
-
-#     def get_id(self):
-#         return self.ten
-
-#     def set_id(self, ngaysinh):
-#         self.ngaysinh = ngaysinh
-
-#     def get_id(self):
-#         return self.ngaysinh
-
-#     def set_id(self, sdt):
-#         self.sdt = sdt
-
-#     def get_id(self):
-#         return self.sdt
-
-#     def set_id(self, email):
-#         self.email = email
-
-#     def get_id(self):
-#         return self.email
-
-#     def set_id(self, gioitinh ):
-#         self.gioitinh = gioitinh
-
-#     def get_id(self):
-#         return self.gioitinh
-
-#     def set_id(self, diachi):
-#         self.diachi = diachi
-
-#     def get_id(self):
-#         return self.diachi
-
-#     def show_User(self):
-#         # print("\n+--------------------+")
-#         print(f"|Tên khách hàng                 :          {self.ten}   |")
-#         print(f"|ngày sinh khách hàng           :          {self.ngaysinh}   |")
-#         print(f"|số điện thoại khách hàng       :          {self.sdt}   |")
-#         print(f"|email khách hàng                 :          {self.email}   |")
-#         print(f"| giới tính khách hàng          :          {self.gioitinh}   |")
-#         print(f"| địa chỉ khách hàng          :          {self.diachi}   |")
-#         print("+----------------------------------------------+")
-
-# a = User(" ĐÀO ", "06-06-2001 ", "12343252523","sada@gmail.com", "NAM  " , "NAM ĐỊNH" )
-# b = User(" HOA ", "01-01-2003" , "33214321442","324234@gmail.com","NỮ ", " VIỆT NAM ")
-# print(a.show_User())
-# print(b.show_User())
+                tmp_nam_sxuat = input(lable_input)
+                tmp_nam_sxuat = ValidateAmountInputForm(tmp_nam_sxuat, lable_input,1900, datetime.now().year)
+                dienthoai.set_nam_sxuat(tmp_nam_sxuat)
+                #Them vao dictionary DanhSachSanPham voi key la id cua san pham
+                DanhSachSanPham[dienthoai.get_id()].append(dienthoai)
+                print('----------------------------')
+                # for item in dienthoai.get_list_thongtin_dienthoai():
+                #     print(item)
+                # print('----------------------------')
+                print(DanhSachSanPham)
+                print('----------------------------')
 
 nhapDienThoai()
